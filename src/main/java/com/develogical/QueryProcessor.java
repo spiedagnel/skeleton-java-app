0f7ce27b53f856e9173edfc301fbfe2c2fadae6f
samuel <samuel.piedagnel@googlemail.com>
2020-06-24 15:42:52 +0100

    multiplied

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -13,6 +13,10 @@ public class QueryProcessor {
                 String[] addition = query.split("what is ");
                 String[] numbers = addition[1].split(" plus ");
                 return String.valueOf(Integer.valueOf(numbers[0]) + Integer.valueOf(numbers[1]));
+            }else if (query.toLowerCase().contains(" multiplied by ")){
+                String[] addition = query.split("what is ");
+                String[] numbers = addition[1].split(" multiplied by ");
+                return String.valueOf(Integer.valueOf(numbers[0]) * Integer.valueOf(numbers[1]));
             }
 
         }else if (query.toLowerCase().contains("which of the following numbers is the largest")) {
